be387cae9fa0e08b4b02920cad9e82a328102e3d
mrsOwl <whitecapricorni@gmail.com>
2017-06-11 19:35:29 +0300

    first refactoring

--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -6,23 +6,23 @@ module StateAndBehaviour
   class Car
     attr_reader :color, :model, :year, :current_speed
 
-    def initialize(car)
-      @year = car[:year] || 2015
-      @color = car[:color] || 'purple'
-      @model = car[:model] || 'skyline'
+    def initialize(car_options)
+      @year = car_options.fetch(:year, 2015)
+      @color = car_options.fetch(:color, 'purple')
+      @model = car_options.fetch(:model, 'skyline')
       @current_speed = 0
-    end
-
-    def push_break(x)
-      @current_speed -= x if @current_speed >= x
-    end
-
-    def speed_up(speed)
-      @current_speed += speed
     end
 
     def self.default_car
       new({})
+    end
+
+    def push_break(velocity)
+      @current_speed -= velocity if @current_speed >= velocity
+    end
+
+    def speed_up(velocity)
+      @current_speed += velocity
     end
   end
 end
