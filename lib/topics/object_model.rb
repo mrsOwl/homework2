ea1f33aade350278e193f727283284811e245e4d
mrsOwl <whitecapricorni@gmail.com>
2017-06-11 19:14:21 +0300

    homework2

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -2,4 +2,26 @@
 
 # exercise object model
 module ObjectModel
+  # Linux friendly
+  module LinuxFriendly
+    def fork_process
+      super
+    end
+  end
+
+  # Computer class
+  class Computer
+    def fork_process
+      'Parent: allocate memory'
+    end
+
+    def mine_bitcoins
+      inspect
+    end
+  end
+
+  # Inherits Computer
+  class Laptop < Computer
+    prepend ObjectModel::LinuxFriendly
+  end
 end
--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -2,4 +2,27 @@
 
 # exercise state and behaviour
 module StateAndBehaviour
+  # Car behaviour
+  class Car
+    attr_reader :color, :model, :year, :current_speed
+
+    def initialize(car)
+      @year = car[:year] || 2015
+      @color = car[:color] || 'purple'
+      @model = car[:model] || 'skyline'
+      @current_speed = 0
+    end
+
+    def push_break(x)
+      @current_speed -= x if @current_speed >= x
+    end
+
+    def speed_up(speed)
+      @current_speed += speed
+    end
+
+    def self.default_car
+      new({})
+    end
+  end
 end
